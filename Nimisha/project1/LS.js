28cb97ab3f03d7d06efc1abdf170911fc55cbc99
Nimisha-web <69744730+Nimisha-web@users.noreply.github.com>
2022-04-12 16:06:56 +0530

    Add files via upload

---
+++ Nimisha/project1/LS.js
@@ -0,0 +1,31 @@
+
+const loginForm = document.querySelector("form.login");
+const signupForm = document.querySelector("form.signup");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector(".signup-link a");
+const loginText = document.querySelector(".title-text .login");
+const signupText = document.querySelector(".title-text .signup");
+
+signupBtn.onclick = (()=>{
+    loginForm.style.marginLeft = "-50%";
+    loginText.style.marginLeft = "-50%";
+
+});
+loginBtn.onclick = (()=>{
+    loginForm.style.marginLeft = "0%";
+    loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (()=>{
+    signupBtn.click();
+    return false;
+});
+
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#id_password');
+togglePassword.addEventListener('click', function (e) { 
+     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    this.classList.toggle('fa-eye-slash');
+});    
